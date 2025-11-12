ec1f0681d0a0904ea3700fb966fb09a0669df734
Ian Wulf <ianwulf24@gmail.com>
2025-11-12 11:05:22 -0600

    App Controller Module -Added a module to coordinate between intenal logic modules and the DOM/UI -Initializes todo list -Initializes/renders the DOM/UI -Can add a project and render it to DOM

---
+++ src/appcontroller.js
@@ -0,0 +1,22 @@
+import TodoList from "./todolist.js";
+import Project from "./project.js";
+
+export default function AppController() {
+    // initialize todolist
+    const todoList = ToDoList();
+
+    // initialize DOM
+    function init() {
+
+    }
+
+    // be able to add project to list and render it
+    // on DOM
+    function addProject(name) {
+        todoList.addProject(Project(name));
+        // render to DOM
+
+    }
+
+    return { init, addProject };
+}
